cb978176d79f4016d7617c85e84701fd8cc1038f
dotunoflagos <adedotun_balogun@yahoo.com>
2022-11-28 09:22:54 +0100

    Console exex

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,12 +4,14 @@ import json
 from models.base_model import BaseModel
 
 
-classes = {"BaseModel":BaseModel}
+classes = {"BaseModel": BaseModel}
+
+
 class FileStorage:
     """serializes instances to a JSON file & deserializes back to instances"""
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         """returns the dictionary __objects"""
         if self.__objects is not None and len(self.__objects) != 0:
@@ -18,13 +20,13 @@ class FileStorage:
                 newdic[key] = value
             return newdic
         return self.__objects
-    
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
-        
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
@@ -51,6 +53,7 @@ class FileStorage:
             with open(self.__file_path, 'r') as f:
                 str = json.load(f)
             for key in str:
-                self.__objects[key] = classes[str[key]["__class__"]](**str[key])
-        except:
+                self.__objects[key] = \
+                    classes[str[key]["__class__"]](**str[key])
+        except Exception:
             pass
--- models/user.py
+++ models/user.py
@@ -6,13 +6,12 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """user class"""
-    
+
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
     def __init__(self, *args, **kwargs):
         """initializes user"""
-        super().__init__(*args, **kwargs)        
-a = User()
-print(a.email)
+        super().__init__(*args, **kwargs)
